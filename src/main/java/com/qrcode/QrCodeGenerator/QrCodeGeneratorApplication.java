5ac9a00e4e338878cdce9b94709887ac03a7a0d8
ravaeusz <girardirafael2003@gmail.com>
2025-12-29 13:41:45 -0300

    firts commit

---
+++ src/main/java/com/qrcode/QrCodeGenerator/QrCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.qrcode.QrCodeGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeGeneratorApplication.class, args);
+	}
+
+}
